3494c4e8c476a90c9daa9cdd8c0ca7819c2cff5c
Palaniappan <173454@cognizant.com>
2017-12-07 16:39:37 +0100

    updated user mgt service with log4j

--- user-mgt-service/src/main/java/com/cts/user/service/UserManagementServiceApplication.java
+++ user-mgt-service/src/main/java/com/cts/user/service/UserManagementServiceApplication.java
@@ -1,15 +1,19 @@
 package com.cts.user.service;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.cts.user.service.repository")
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class UserManagementServiceApplication {
+	private static final Logger logger = LogManager.getLogger(UserManagementServiceApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagementServiceApplication.class, args);
