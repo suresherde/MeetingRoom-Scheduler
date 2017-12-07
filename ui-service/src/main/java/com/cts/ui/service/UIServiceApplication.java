601c223ed76403d64dc3b20cb51f34e1897aca9f
Palaniappan <173454@cognizant.com>
2017-12-07 12:25:27 +0100

    added code to register to eureka

--- ui-service/src/main/java/com/cts/ui/service/UIServiceApplication.java
+++ ui-service/src/main/java/com/cts/ui/service/UIServiceApplication.java
@@ -2,10 +2,12 @@ package com.cts.ui.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UIServiceApplication {
 
 	public static void main(String[] args) {
