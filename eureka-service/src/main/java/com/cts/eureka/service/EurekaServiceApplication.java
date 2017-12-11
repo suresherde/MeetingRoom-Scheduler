5c3ee71313ab7001e208abde07cdf14d1f9d3603
Palaniappan <173454@cognizant.com>
2017-12-11 16:35:57 +0100

    uopdated code for CORS

--- eureka-service/src/main/java/com/cts/eureka/service/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/cts/eureka/service/EurekaServiceApplication.java
@@ -2,8 +2,12 @@ package com.cts.eureka.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.cts.eureka.service.filter.CORSFilter;
 
 @EnableZuulProxy
 @EnableEurekaServer
@@ -13,5 +17,15 @@ public class EurekaServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
+	
+	 @Bean
+	    public FilterRegistrationBean corsFilterRegistration() {
+	        FilterRegistrationBean registrationBean =
+	                new FilterRegistrationBean(new CORSFilter());
+	        registrationBean.setName("CORS Filter");
+	        registrationBean.addUrlPatterns("/*");
+	        registrationBean.setOrder(1);
+	        return registrationBean;
+	    }
 }
 
